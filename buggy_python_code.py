4c3cc56bc36214bcee3a6da2e42edc41c6025ead
Tomáš Nevrlka <tnevrlka@redhat.com>
2025-04-30 18:42:30 +0200

    Fix urllib import

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -27,9 +27,10 @@ def print_nametag(format_string, person):
 
 def fetch_website(urllib_version, url):
     # Import the requested version (2 or 3) of urllib
-    exec(f"import urllib{urllib_version} as urllib", globals())
-    # Fetch and print the requested URL
+    if urllib_version in (2, 3):
+        __import__("urllib" + urllib_version)
 
+    # Fetch and print the requested URL
     try:
         http = urllib.PoolManager()
         r = http.request("GET", url)
